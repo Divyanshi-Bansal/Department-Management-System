b5b8a5c41c3b493b20e9a60e1611a9e595a240da
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2021-05-21 22:32:55 +0530

    issue resolved

--- dist/controllers/index_controllers.js
+++ dist/controllers/index_controllers.js
@@ -1,20 +1,32 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.errorPage = exports.delete_employees = exports.post_employees = exports.put_employees = exports.get_home = exports.get_employees = void 0;
-const get_employees = (req, res, next) => {
+const database_1 = require("../database");
+const get_employees = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
+        const response = yield database_1.pool.query('SELECT * FROM empdept ORDER BY empid');
         return res.status(200).json({
-            message: "Divyanshi Bansal",
+            entries: response.rows,
             time: new Date()
         });
     }
-    catch (_a) {
-        return res.status(404).json({
-            message: "Page can't found",
+    catch (e) {
+        console.log(e);
+        return res.status(500).json({
+            error: 'Internal Server error',
             time: new Date()
         });
     }
-};
+});
 exports.get_employees = get_employees;
 const get_home = (req, res, next) => {
     try {
--- src/controllers/index_controllers.ts
+++ src/controllers/index_controllers.ts
@@ -1,16 +1,23 @@
-export const get_employees = (req , res , next) =>{
-    try{
+import { Request, Response } from 'express';
+import { pool } from '../database';
+import { QueryResult } from 'pg';
+
+
+export const get_employees = async (req: Request, res: Response): Promise<Response> =>{
+    try {
+        const response: QueryResult = await
+            pool.query('SELECT * FROM empdept ORDER BY empid');
         return res.status(200).json({
-            message:"Divyanshi Bansal",
+            entries:response.rows,
+            time:new Date()
+        });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({
+            error:'Internal Server error',
             time: new Date()
-        })
-    }
-    catch{
-        return res.status(404).json({
-            message: "Page can't found",
-            time : new Date()
-        })
-    }
+        });
+    } 
 }
 
 export const get_home = (req , res , next) =>{
@@ -77,4 +84,22 @@ export const errorPage=(req,res,next)=>{
             error:"Some error got found ",
             time:new Date()
     })
+}
+
+export const get_employeesByID = async (req: Request, res: Response): Promise<Response> =>{
+    const id = req.params.id;
+    try {
+        const response: QueryResult = await
+            pool.query('SELECT * FROM empdept ORDER BY empid WHERE empid==id');
+        return res.status(200).json({
+            entries:response.rows,
+            time:new Date()
+        });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({
+            error:'Internal Server error',
+            time: new Date()
+        });
+    } 
 }
